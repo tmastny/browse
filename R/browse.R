8d15eb66c63f3d2430f418cc9d4f9634ce8b44af
Timothy Mastny <tim.mastny@gmail.com>
2019-12-12 19:55:08 -0600

    browse

--- R/browse.R
+++ R/browse.R
@@ -1,14 +1,19 @@
 #' Format Github
 #'
 #' Formats Github URL based on the repo in the current working directory.
+#' Uses the current HEAD of the git repo.
 #'
 #' @param path The relative path of the file from the top of the git repo.
 #'
 #' @examples
-#' github_url("R/install.R#L29")
+#' github_url("R/browse.R#L6-L9")
+#' github_url("R/browse.R#L6")
 #'
+#' # opens URL in default browser
+#' \dontrun{
 #' github_url("R/install.R") %>%
 #'   browseURL()
+#' }
 #'
 #' @export
 github_url <- function(path) {
@@ -29,4 +34,16 @@ github_url <- function(path) {
 print.github_url <- function(x, ...) {
   cat(x)
   invisible(x)
+}
+
+#' Opens URL in Default Browser
+#'
+#' Takes a file path (with option line numbers) and opens the corresponding
+#' Github permalink in your default browser.
+#'
+#' @inheritParams github_url
+#' @export
+browse <- function(path) {
+  github_url(path) %>%
+    browseURL()
 }
